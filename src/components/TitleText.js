ff6a6dfc23bd03cb339f6f899ab4810de7b833d9
leomorton <leohmorton@gmail.com>
2022-10-21 18:46:58 +0100

    new title

--- src/components/TitleText.js
+++ src/components/TitleText.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../css/TitleText.css';
 
 function TitleText() {
-    return <h1 className="TitleText">TITLE TEXT</h1>;
+    return <h1 className="TitleText">LEO MORTON</h1>;
 }
 
 export default TitleText;
