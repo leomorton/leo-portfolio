0d3676503a089e4083588bcf092262cf76e6a121
leo <leo@ayda.co.uk>
2022-10-24 22:42:42 +0100

    sorting out project rows

--- components/ProjectRow.jsx
+++ components/ProjectRow.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import styles from '../styles/Home.module.scss';
 
-function ProjectRow() {
-  return (
-    <div>ProjectRow</div>
-  )
+function ProjectRow({ projectData }) {
+    const imgStyle = {
+        margin: '0px 10px'
+    }
+
+    return (
+        <div className={styles.projectRow}>
+            <h3 className={styles.projectTitle}>{projectData.name}</h3>
+            <Image className={styles.projectThumbnail} layout='fixed' src={projectData.imagesrc} style={imgStyle} width='200' height='100' alt={projectData.name + ' image'} />
+            <p className={styles.projectRowText}>~</p>
+            <h3 className={styles.clientName}>{projectData.client}</h3>
+        </div>
+    );
 }
 
-export default ProjectRow
+export default ProjectRow;
--- components/ProjectSection.jsx
+++ components/ProjectSection.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import styles from '../styles/Home.module.scss'
+import React from 'react';
+import styles from '../styles/Home.module.scss';
 import ProjectRow from './ProjectRow';
-
 
 function ProjectSection({ sectionTitle, projects }) {
     const rows = [];
 
     projects.forEach((project) => {
-        rows.push(<ProjectRow projectName={project.name} key={project.key.toString()} />)
-    })
-  return (
-      <div className={styles.ProjectSection}>
-          <h2>{sectionTitle}</h2>
-          <div className={styles.SectionTitle}>{rows}</div>
-    </div>
-  )
+        rows.push(<ProjectRow projectData={project} key={project.key.toString()} />);
+    });
+    return (
+        <div className={styles.ProjectSection}>
+            <h2 className={styles.SectionTitle}>{sectionTitle}</h2>
+            <div>{rows}</div>
+        </div>
+    );
 }
 
-export default ProjectSection
+export default ProjectSection;
