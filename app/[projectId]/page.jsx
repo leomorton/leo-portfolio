f7c8c72e70073f034a045e84f27fe50737a3ed67
leo <leo@ayda.co.uk>
2022-12-23 17:40:37 +0000

    adding static params

--- app/[projectId]/page.jsx
+++ app/[projectId]/page.jsx
@@ -1,7 +1,15 @@
-export default function Page() {
+export default function Page({ params }) {
     return (
         <div className="mx-auto max-w-screen-xl">
-            <h1>it&apos;s a project!</h1>
+            <h1 className="text-2xl md:text-5xl">name {params.projectId}</h1>
+            <p>Wheres the text?</p>
         </div>
     );
+}
+
+// run at build time
+export async function generateStaticParams() {
+    const posts = await getPosts(); // request to an API or CMS
+
+    return posts.map((post) => ({ slug: post.slug }));
 }
