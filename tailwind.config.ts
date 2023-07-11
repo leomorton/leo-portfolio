9eedfb16f6460a0af6db05fb53deb21ca1f20800
leomorton <leohmorton@gmail.com>
2023-07-11 18:16:25 +0100

    added responsive title

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+import theme from "tailwindcss/defaultTheme";
+
 import { type Config } from "tailwindcss";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      halogen: ["halogen", "sans-serif"],
+      test: ["Arial"],
+    },
+    screens: {
+      tiny: {'max': '200px'},
+      ...theme.screens,
+    },
   },
   plugins: [],
 } satisfies Config;
