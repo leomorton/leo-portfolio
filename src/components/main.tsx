d6e12ba0ee1d9c3c90a4a999388305757ca100a9
Leo Morton <leohmorton@gmail.com>
2024-01-17 13:29:58 +0000

    added pdf placeholder

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Main(props: React.PropsWithChildren) {
   return (
-    <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#f2f2f2] to-[#ccbfb3]">
+    <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#e0e0e0] to-[#BFB8B2]">
       {props.children}
     </main>
   );
--- src/components/title.tsx
+++ src/components/title.tsx
@@ -9,7 +9,7 @@ function Title(props: TitleProps) {
   return (
     <div className="relative">
       <CornerBorders />
-      <h1 className="whitespace-nowrap px-[3vw] py-[2vw] font-halogenRegular text-[8.5vw] font-medium leading-none text-neutral-700 tiny:text-[7vw] sm:px-[2vw] sm:py-[1vw] sm:text-[9vw] xl:text-9xl">
+      <h1 className="whitespace-nowrap px-[3vw] py-[2vw] font-halogenRegular text-[8.5vw] font-medium leading-none text-orange-500 tiny:text-[7vw] sm:px-[2vw] sm:py-[1vw] sm:text-[9vw] xl:text-9xl">
         {props.title}
       </h1>
     </div>
